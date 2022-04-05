e766b21a952c1cbdf66c74be5f634402626af48b
dudushy <eduardotalarico1337@gmail.com>
2022-04-05 08:37:19 -0300

    AS tweaks

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { HTTP } from '@ionic-native/http';
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +9,30 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(
+    private http: HTTP
+    ) {}
 
+  test(email, password){
+
+    let url = 'http://mpi.spedo.com.br/api/app/logarAPP.json';
+
+    let data = {
+      str_email: btoa(email),
+      str_senha: btoa(password),
+    };
+
+    let header = {
+        'MPI-API-KEY': '0000000000000000000000000000000000000000',
+      }
+
+    this.http.setDataSerializer('json');
+
+    this.http.post(url, data, header)
+      .then(data => {
+        var response = JSON.parse(data.data);
+        console.log(response)
+      }
+    )
+  }
 }
